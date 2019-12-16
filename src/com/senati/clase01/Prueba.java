4c7855b23286ae878e5d3637bac6c049729a0cb1
Alumno <Alumno@DESKTOP-QK63PLK>
2019-12-16 11:00:22 -0500

    Envio del proyecto al 50%

---
+++ src/com/senati/clase01/Prueba.java
@@ -0,0 +1,36 @@
+package com.senati.clase01;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Programa creado en la ETI
+		 * Autor: Carlos Ramos
+		 */
+		//Identificadores de tipo de datos
+		//long, int, short, byte, float, double, char, boolean
+		//Clase String
+		//	Altura de una persona en metros  float
+		float altura1 = 0f;
+		//	Altura de una persona en centímetros  short
+		short altura2 = 165;
+		//	Número del DNI String
+		String dni = "25771858";
+		//	Edad de una persona byte
+		byte edad = 25;
+		//	Sexo de una persona char o boolean
+		char sexo1 = 'm';
+		boolean sexo2 =true;
+		
+		//Impresion en consola
+		System.out.println("Altura en metros ->" + altura1);
+		System.out.println("Altura en centimetros ->" +altura2);
+		System.out.println("Docuemnto de identidad ->" +dni);
+		System.out.println("Edad ->" + edad);
+		System.out.println("sexo -> " +sexo1);
+		System.out.println("sexo ->" +sexo2);
+		System.out.println("------------");
+	}
+
+}
